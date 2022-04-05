af4def71dd45974131be6d5fa66c31528f70c7d0
cryduswjd <cryduswjd@naver.com>
2022-04-05 12:02:13 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,9 +14,20 @@ function login() {
 
     fetch("/login", {
         method: "POST",
-        header: {
+        headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
-    });
+        body: JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if(res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);   
+        }
+    })
+    .catch((err) => {
+        console.error("로그인 중 에러 발생");
+    })
 }
